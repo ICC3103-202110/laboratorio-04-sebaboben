ed0324abe0755107207d41c41b5be7159477ba15
sebastian bobenrieth <sjbobenrieth@miuandes.cl>
2021-05-10 02:08:16 -0400

    tarea terminada

---
+++ tarea1.js
@@ -0,0 +1,45 @@
+const ps = require("prompt-sync");
+const prompt = ps();
+
+
+function update(msg,counter){
+    if(msg === "+"){
+        view(counter+1);
+        return counter+1;
+    }
+    else if(msg === "-"){
+        view(counter-1);
+        return counter-1;
+    }
+    else{
+        return counter;
+    };
+};
+
+function view(counter){
+    return "Count: " +counter+"\n\n(+)      (-)\n\n(q) for quit\n";
+};
+
+function app(counter){
+    
+    while (true){
+        let currentView = view(counter);
+        console.clear();
+
+        console.log(currentView);
+        let msg = prompt("What would you do: ")
+        if (msg === "q"){
+            return 0;
+        } 
+        else{
+            counter = update(msg,counter);
+        };
+    };
+};
+
+
+
+
+
+
+app(0)
